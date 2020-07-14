d091644074044f38674b26285bcae40b0c6ad1b5
thanaponp <thanaponp@gmail.com>
2020-07-14 17:46:54 +0700

    Update uptimerobot-line.php
    Able show sticker

--- uptimerobot-line.php
+++ uptimerobot-line.php
@@ -1,14 +1,21 @@
 <?php
-function send_line_notify($message, $token)
+function send_line_notify($params, $token)
 {
+  $message_data = array(
+    'message' => $params["message"],
+    'stickerPackageId' => $params["stickerPkg"],
+    'stickerId' => $params["stickerId"],
+  );
+
   $ch = curl_init();
   curl_setopt( $ch, CURLOPT_URL, "https://notify-api.line.me/api/notify");
   curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0);
   curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0);
   curl_setopt( $ch, CURLOPT_POST, 1);
-  curl_setopt( $ch, CURLOPT_POSTFIELDS, "message=$message");
+  curl_setopt( $ch, CURLOPT_POSTFIELDS, $message_data);
   curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-  $headers = array( "Content-type: application/x-www-form-urlencoded", "Authorization: Bearer $token", );
+  //$headers = array( "Content-type: application/x-www-form-urlencoded", "Authorization: Bearer $token", );
+  $headers = array("Content-type: multipart/form-data", "Authorization: Bearer $token");
   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
   curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
   $result = curl_exec( $ch );
@@ -25,9 +32,26 @@ $alertTypeFriendlyName = $_GET['alertTypeFriendlyName'];
 $alertDetails = $_GET['alertDetails'];
 
 if( $monitorFriendlyName != "" )
-	$message = "$monitorFriendlyName $alertTypeFriendlyName ($alertDetails)";
+  $message = "$monitorFriendlyName $alertTypeFriendlyName ($alertDetails)";
 else
-	$message = "OK";
-send_line_notify($message, $token);
+  $message = "OK";
+
+if( $alertTypeFriendlyName == "Up" )
+{
+  $stickerPkg = 3;
+  $stickerId = 184;
+} 
+else
+{
+  $stickerPkg = 3;
+  $stickerId = 188;
+}
+  
+$params = array(
+  "message"        => $message, //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
+  "stickerPkg"     => $stickerPkg, //stickerPackageId
+  "stickerId"      => $stickerId, //stickerId
+);
+send_line_notify($params, $token);
 
 ?>
